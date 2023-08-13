04a717cdb3de0c1c0ac64b4374a85f257118c31e
Henhrie <akinolahenryayobami@gmail.com>
2023-08-13 21:29:34 +0800

    test

--- shell_loop.c
+++ shell_loop.c
@@ -5,17 +5,21 @@
  */
 void shell_loop(void)
 {
-    char *line;
-    char **args;
-    int status;
+char *line;
+char **args;
+int status;
 
-    do {
-        printf("($) ");
-        line = read_line();
-        args = parse_line(line);
-        status = execute_command(args);
+do
+{
+printf("($) ");
+line = read_line();
+args = parse_line(line);
+status = execute_command(args);
 
-        free(line);
-        free(args);
-    } while (status);
+free(line);
+free(args);
+}
+
+while (status);
+
 }
